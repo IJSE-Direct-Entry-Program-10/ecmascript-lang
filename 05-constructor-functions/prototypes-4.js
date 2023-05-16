a12984a3e0066949e57dd97bbcb07c83714ef9b6
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-05-16 14:48:54 +0530

    Finish

--- 05-constructor-functions/prototypes-4.js
+++ 05-constructor-functions/prototypes-4.js
@@ -1,8 +1,8 @@
-function abstractFn(){
+function abstractFn() {
     throw new Error("Not implemented yet");
 }
 
-function User(username, password){
+function User(username, password) {
     if (new.target) throw new Error("Abstract constructors can't be instantitated with out implementing");
     if (this == globalThis) throw new Error("Abstract constructors can be invoked only via sub class constructors");
 
@@ -15,24 +15,54 @@ User.prototype.logout = abstractFn;
 
 User.prototype.SOME_RANDOM_INT = parseInt(Math.random() * 10);
 
-function Employee(username, password){
+/* If the property is final */
+const descriptor = Object.getOwnPropertyDescriptor(User.prototype, 'SOME_RANDOM_INT');
+descriptor.writable = false;
+Object.defineProperty(User.prototype, 'SOME_RANDOM_INT', descriptor);
+
+function Employee(username, password) {
     if (new.target) throw new Error("Abstract constructors can't be instantitated with out implementing");
-    if (this == globalThis) throw new Error("Abstract constructors can be invoked only via sub class constructors");    
+    if (this == globalThis) throw new Error("Abstract constructors can be invoked only via sub class constructors");
     User.call(this, username, password);
 }
 
+Employee.prototype.login = function () {
+    console.log("Employee logged in");
+}
+
+Employee.prototype.logout = function () {
+    console.log("Employee logged out");
+}
+
+Employee.prototype.sendEmails = abstractFn;
+
 Object.setPrototypeOf(Employee.prototype, User.prototype);
 
-function Manager(username, password){
+function Manager(username, password) {
     if (!new.target) throw new Error("Constructor functions should be invoked with the new operator");
     Employee.call(this, username, password);
 }
 
+Manager.prototype.sendEmails = function () {
+    console.log("Manager is sending emails");
+}
+
 Object.setPrototypeOf(Manager.prototype, Employee.prototype);
 
-function Assistant(username, password){
+function Assistant(username, password) {
     if (!new.target) throw new Error("Constructor functions should be invoked with the new operator");
     Employee.call(this, username, password);
 }
 
-Object.setPrototypeOf(Manager.prototype, Employee.prototype);
+Assistant.prototype.sendEmails = function () {
+    console.log("Asisstant is sending emails");
+}
+
+Object.setPrototypeOf(Assistant.prototype, Employee.prototype);
+
+console.log("-----------------------------");
+
+let emp = new Manager();
+emp.login();
+emp.sendEmails();
+emp.logout();
